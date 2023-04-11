63e9aea431f506a953a41c0f0ba1022609e35f3c
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-04-11 14:54:35 +0600

    All Image folder added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='text-center mt-10 text-5xl font-bold'>
-            <h1>This is home page</h1>
-        </div>
+        <>
+            <section className='flex justify-evenly w-80px'>
+                <div>
+                    <h1 className='text-5xl font-bold text-gray-500'>One Step <br />Closer To Your <br /><span className='text-sky-500'>Dream Job</span></h1>
+                    <h4 className=''>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</h4>
+                    <button className='btn-primary pt-5'>Get Started</button>
+                    
+                </div>
+            </section>
+        </>
     );
 };
 
