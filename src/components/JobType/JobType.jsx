a213ab102a64c63a101eb92f132afb641f0905ae
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-04-12 14:21:50 +0600

    Assign Marks By TinyAreaChart Added

--- src/components/JobType/JobType.jsx
+++ src/components/JobType/JobType.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './JobType.css'
 const JobType = ({job}) => {
     const {logo, position, jobName} = job
     return (
-        <div>
+        <div className= 'bg-sky-100 p-4 mx-8 mt-8'>
             <img src={logo} alt="logo" />
             <h1 className='font-semibold'>{jobName}</h1>
             <h1>{position}</h1>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
-
+import { AreaChart, Area, XAxis, YAxis, Tooltip,} from 'recharts';
 const Statistics = () => {
+    const marksData = [
+        {
+            name: 'Assignment-1',
+            mark: 57,
+        },
+        {
+            name: 'Assignment-2',
+            mark: 56,
+        },
+        {
+            name: 'Assignment-3',
+            mark: 49,
+        },
+        {
+            name: 'Assignment-4',
+            mark: 59,
+        },
+        {
+            name: 'Assignment-5',
+            mark: 57,
+        },
+        {
+            name: 'Assignment-6',
+            mark: 47,
+        },
+        {
+            name: 'Assignment-7',
+            mark: 60,
+        },
+        {
+            name: 'Assignment-8',
+            mark: 38,
+        }
+    ];
     return (
-        <div className='text-center mt-10 text-5xl font-bold'>
-            <h1>This is Statistics Page</h1>
+        <div>
+            <h3 className='text-center font-bold text-4xl text-sky-600 mt-10'>Assignment Marks By TinyAreaChart</h3>
+                <AreaChart width={1200}
+                    height={400}
+                    data={marksData}
+                    margin={{
+                        top: 50,
+                        right: 5,
+                        left: 50,
+                    }}>
+                    <XAxis dataKey="name"></XAxis>
+                    <YAxis dataKey=""></YAxis>
+                    <Area type="monotone" dataKey="mark" stroke="#E74646" fill="#9A208C" />
+                    <Tooltip></Tooltip>
+                </AreaChart>
         </div>
     );
 };
